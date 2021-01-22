9c4a4d8d9c37fcea05168c3d5796bc7def798777
harshjohar <hpsj3112@gmail.com>
2021-01-22 18:45:36 +0530

    day 7_corrected

--- dsa1/fctrl.cpp
+++ dsa1/fctrl.cpp
@@ -6,32 +6,15 @@ int number(int n);
 int main() {
     int t;
     cin>>t;
-    int arr[t];
-    int out[t]={0};
-    for(int i=0; i<t; i++) {
-        cin>>arr[i];
-    }
-    // int p = 5;
-    // int c = 1;
-    for(int j=0; j<t; j++) {
-        for(int k=1; k<=arr[j]; k++) {
-            if(k%5 == 0) {
-                out[j] = out[j] + number(k);
-            }
+    while(t--) {
+        long n;
+        cin>>n;
+        long count = 0;
+        while(n!=0) {
+            count+=n/5;
+            n = n/5;
         }
-    }
-    for(int i=0; i<t; i++) {
-        cout<<out[i]<<endl;
+        cout<<count<<endl;
     }
     return 0;
-}
-
-int number(int n) {
-    // number of 5's in the given number
-    int count = 0;
-    while(n>1 && n%5==0) {
-        count++;
-        n = n/5;
-    }
-    return count;
 }

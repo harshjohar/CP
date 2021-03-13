58f0d948a8542b121d0390e6a9f4a177750b0939
Harshpreet Singh Johar <50266759+harshjohar@users.noreply.github.com>
2021-03-14 03:32:05 +0530

    day 32

--- uass001/mineat.py
+++ uass001/mineat.py
@@ -1,38 +1,28 @@
+from math import ceil
+def is_ans(arr, k, h):
+    count = 0
+    for i in arr:
+        count+=ceil(i/k)
+    if count<=h:
+        return True
+    else:
+        return False
+
 t = int(input())
-
-def good(arr, k, n):
-    # count the hrs required for a particular k
-    count=0
-    li = arr[:]
-    for i in range(n):
-        while li[i] > 0:
-            li[i]-=k
-            count+=1
-    # print('y', count, k)
-    # print(arr, li)
-    return count
-
-def binary(li, n, h):
-    lo = 0
-    hi = n
-
-    while lo < hi:
-        mid = lo + (hi-lo)//2
-        # print(mid)
-        if good(li, li[mid], n) == h:
-            # print('e')
-            # print(li[mid])
-            return li[mid]
-        elif good(li, li[mid], n) > h:
-            lo = mid+1
-        else:
-            hi = mid-1
-    return -1
-
 for _ in range(t):
     n, h = map(int, input().split())
-    data = list(map(int, input().split()))
-    data.sort()
-    ans = binary(data, n, h)
+    li = list(map(int, input().split()))
+
+    lo = 1
+    hi = max(li)
+
+    while lo <= hi:
+        mid = lo + (hi-lo)//2
+
+        if is_ans(li, mid, h):
+            ans = mid
+            hi=mid-1
+
+        else:
+            lo=mid+1
     print(ans)
-    

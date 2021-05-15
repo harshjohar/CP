00c7cdfebe407712ed8fb8d3766837fca7bb7a74
Harshpreet Singh Johar <50266759+harshjohar@users.noreply.github.com>
2021-05-16 00:23:06 +0530

    Day1

---
+++ 1504a.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t; cin>>t;
+    while(t--) {
+        string s;
+        cin>>s;
+        int flag = 0;
+        
+    }
+}
